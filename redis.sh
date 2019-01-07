feb32055f12064e6423afc2ea86e5a96013f9d63
jingyihome <webmaster@loveyan.com>
2019-01-07 15:46:24 +0800

    Create redis.sh
    Redis 多端口管理脚本

---
+++ redis.sh
@@ -0,0 +1,65 @@
+####################################################################
+# File Name: redis.sh
+# Author: jingyihome
+# mail: webmaster@loveyan.com
+####################################################################
+#!/bin/bash
+REDISPORT=$1
+PASSWD='Test2018!'
+HOSTS="127.0.0.1"
+EXEC=/usr/local/webserver/redis/redis-server
+CLIEXEC=/usr/local/webserver/redis/redis-cli
+CONF="/usr/local/webserver/redis/${REDISPORT}.conf"
+
+function exec_start_redis(){
+    echo "start redis service..."
+      ${EXEC} ${CONF}
+}
+
+function exec_stop_redis(){
+    echo "stop redis service..."
+      ${CLIEXEC} -h ${HOSTS} -p ${REDISPORT} -a ${PASSWD} shutdown
+}
+
+function exec_uptime_redis(){
+    ${CLIEXEC} -h ${HOSTS} -p ${REDISPORT} -a ${PASSWD} info|grep uptime_in_days|awk -F":" '{print $NF}'
+}
+
+function exec_version_redis(){
+    ${CLIEXEC} -h ${HOSTS} -p ${REDISPORT} -a ${PASSWD} info|grep redis_version|awk -F":" '{print $NF}'
+}
+
+# Start
+if [[ $1 == *[!0-9]* ]]||[[ "$1" = "" ]]; then
+    echo "ERROR! Redis port must a number!"
+    exit 1
+fi
+
+action=$2
+[ -z $2 ] && action=help
+case "$action" in
+help)
+    echo "Usage: ./`basename $0` ${REDISPORT} {start|stop|restart}"
+    ;;
+start)
+    exec_start_redis
+    ;;
+stop)
+    exec_stop_redis
+    ;;
+restart)
+    exec_stop_redis
+    sleep 3;
+    exec_start_redis
+    ;;
+uptime)
+    exec_uptime_redis
+    ;;
+version)
+    exec_version_redis
+    ;;
+*)
+    echo "error! [${action}]"
+    echo "Usage: ./`basename $0` ${REDISPORT} {start|stop|restart}"
+    ;;
+esac
