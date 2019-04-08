18e5d2ba24b730bd043eef31e0026296c5168192
jingyihome <webmaster@loveyan.com>
2019-04-08 11:20:53 +0800

    更新elasticsearch_clean.sh文件

--- elasticsearch_clean.sh
+++ elasticsearch_clean.sh
@@ -8,10 +8,12 @@
 # Github: https://github.com/jingyihome           #
 ###################################################
 logfile="/home/ops/elk/elkDailyDel.log"
+elkhost="172.16.0.16"
+elkport="9200"
 if test ! -f "${logfile}" ;then
     touch ${logfile}
 fi
-indices=$(curl -s "172.16.0.16:9200/_cat/indices?v"|grep 'logstash'|awk '{print $3}')
+indices=$(curl -s "${elkhost}:${elkport}/_cat/indices?v"|grep 'logstash'|awk '{print $3}')
 sixtyDaysAgo=$(date -d "$(date "+%Y%m%d") -30 days" "+%s")
 function DelOrNot(){
     if [ $(($1-$2)) -ge 0 ] ;then
@@ -25,7 +27,7 @@ do
     indexDate=`echo ${index}|awk -F'-' '{print $NF}'|sed 's/\./-/g'`
     indexTime=`date -d "${indexDate}" "+%s"`
     if [ `DelOrNot ${indexTime} ${sixtyDaysAgo}` -eq 0 ] ;then
-        delResult=`curl -s -XDELETE "172.16.0.16:9200/${index}"`
+        delResult=`curl -s -XDELETE "${elkhost}:${elkport}/${index}"`
         echo "delResult is ${delResult}" >> ${logfile}
         if [ `echo ${delResult}|grep 'acknowledged'|wc -l` -eq 1 ] ;then
             echo "${index} had already been deleted!" >> ${logfile}
